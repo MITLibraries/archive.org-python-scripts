87bb02e46367368f92901a5daeb4719d26868e4d
Eric Hanson <ehanson@mit.edu>
2019-04-24 10:29:15 -0400

    Update uploadNewMarcxmlRecords.py

--- uploadNewMarcxmlRecords.py
+++ uploadNewMarcxmlRecords.py
@@ -3,6 +3,7 @@ import argparse
 import csv
 import os
 import shutil
+import datetime
 import secrets
 
 # command line arguments
@@ -20,29 +21,36 @@ else:
 c = {'s3': {'access': secrets.access, 'secret': secrets.secret}}
 s = get_session(config=c)
 
+# get row count
 with open(fileName) as csvfile:
     reader = csv.DictReader(csvfile)
     rowCount = len(list(reader))
 
+# create log
+f = csv.writer(open('recordsUpdated.csv', 'w'))
+f.writerow(['iaID'] + ['oclcNum'] + ['response'] + [''])
+
 # script content
 backupDirectory = 'archive.orgBackup/'
 replacementDirectory = 'oclcRecords/'
-if os.path.isdir(replacementDirectory) is False:
-    os.makedirs(replacementDirectory)
+if os.path.isdir('archive.orgBackup/') is False:
+    os.makedirs('replacementDirectory')
 with open(fileName) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        if rowCount == 101:
-            rowCount -= 1
-            print('Items remaining: ', rowCount)
-            iaId = row['iaIdentifier']
-            oclcNum = row['oclcNum']
-            item = s.get_item(iaId)
-            fileName = iaId + '_marc.xml'
-            r = item.download(files=fileName, destdir=backupDirectory)
-            print(r)
-            print(oclcNum + '.xml', iaId + '_marc.xml')
-            shutil.copy(replacementDirectory + oclcNum + '.xml',
-                        replacementDirectory + iaId + '_marc.xml')
-            r2 = item.upload(replacementDirectory + iaId + '_marc.xml')
-            print(r2)
+        rowCount -= 1
+        print('Items remaining: ', rowCount)
+        iaId = row['iaIdentifier']
+        oclcNum = row['oclcNum']
+        item = s.get_item(iaId)
+        fileName = iaId + '_marc.xml'
+        r = item.download(files=fileName, destdir=backupDirectory)
+        print(r)
+        print(oclcNum + '.xml', iaId + '_marc.xml')
+        shutil.copy(replacementDirectory + oclcNum + '.xml',
+                    replacementDirectory + iaId + '_marc.xml')
+        r2 = item.upload(replacementDirectory + iaId + '_marc.xml')
+        utc = datetime.datetime.utcnow()
+        utcTime = utc.strftime('%Y-%m-%dT%H:%M:%SZ')
+        f.writerow([iaId] + [oclcNum] + [r2] + [utcTime])
+        print(r2)
